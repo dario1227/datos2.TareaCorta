277755d690df12e339eccf4f8d32a14a88ea405e
kenneth <khken21@gmail.com>
2018-03-17 23:11:18 -0600

    Arreglo Funcion
    Se arreglo la funcion y ya ordena el big array bien guapo,ya se termino esto y se necesita nada mas escribir en ciertas partes del file :DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD

--- main.cpp
+++ main.cpp
@@ -5,11 +5,18 @@
 using namespace std;
 int main() {
     maker::make(3000);
-
-    int array[9];
-    BigArray::ordenar(array,0,300);
+    maker* var = new maker();
+   int array[100];
+   var->getLine(1);
+    QuickSort::Start(array,0,100);
+    int inx = 0;
+    while (inx<100){
+      std::cout<<BigArray::x[inx]<<",";
+       inx++;
+    }
+    BigArray::ordenar(array,0,299);
+    BigArray::print_Array(0);
     BigArray::print_Array(1);
     BigArray::print_Array(2);
-    BigArray::print_Array(3);
     return 0;
 }
